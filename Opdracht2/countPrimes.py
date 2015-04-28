2bfbb21ef5c1741d66147a99ade619b5a6ed5977
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-04-28 12:12:39 +0200

    updated

--- Opdracht2/countPrimes.py
+++ Opdracht2/countPrimes.py
@@ -1,5 +1,30 @@
 import sys
+import math
 
 path=sys.argv[1]
 
 file=open(path,'r')
+
+pi=0
+pi_2=0
+prev='-1'
+C2 = 0.6601618
+
+for line in file:
+    pi+=1
+    if int(line)==int(prev)+2:
+        pi_2+=1
+    prev=line
+
+maxprime=int(prev)
+n=maxprime+1
+
+print('Largest Prime = ',maxprime)
+print('--------------------------------')
+print('pi(N)         = ', pi)
+print('N/log(N)      = ', n/math.log(n))
+print('ratio         = ', pi*math.log(n)/n)
+print('--------------------------------')
+print('pi_2(N)       = ', pi_2)
+print('2CN/log(N)^2  = ', 2*C2*n/math.log(n)**2)
+print('ratio         = ', pi_2*math.log(n)**2/(2*C2*n))
