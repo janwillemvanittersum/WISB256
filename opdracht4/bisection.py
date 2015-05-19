a6dfce06acee05440252a72c168e3269a1df4ec0
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-05-19 22:31:57 +0200

    Revert "opdracht 4 met bonus"
    This reverts commit f5639bc9bb6b50da31b4ac5df8a7ee82fbacb681.

--- opdracht4/bisection.py
+++ opdracht4/bisection.py
@@ -8,13 +8,3 @@ def findRoot(f,a,b,epsilon):
         return findRoot(f,a,m,epsilon)
     else:
         return findRoot(f,m,b,epsilon)
-
-def findAllRoots(f,a,b,epsilon):
-    roots=[]
-    totsteps=abs(b-a)/epsilon
-    for i in range(0,int(totsteps)):
-        if f(a+i*epsilon)*f(a+(i+1)*epsilon)<0:
-            roots.append(a+(i+1/2)*epsilon)
-    if f(a+int(totsteps)*epsilon)*f(b)<0:
-        roots.append((a+b+int(totsteps)*epsilon)/2)
-    return roots
