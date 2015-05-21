0552e3081a3cb37df9f712e904e7f61e010ecef5
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-05-21 13:47:07 +0200

    < -> <=

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -13,8 +13,8 @@ def findAllRoots(f,a,b,epsilon):
     roots=[]
     totsteps=abs(b-a)/epsilon
     for i in range(0,int(totsteps)):
-        if f(a+i*epsilon)*f(a+(i+1)*epsilon)<0:
+        if f(a+i*epsilon)*f(a+(i+1)*epsilon)<=0:
             roots.append(a+(i+1/2)*epsilon)
-    if f(a+int(totsteps)*epsilon)*f(b)<0:
+    if f(a+int(totsteps)*epsilon)*f(b)<=0:
         roots.append((a+b+int(totsteps)*epsilon)/2)
     return roots
