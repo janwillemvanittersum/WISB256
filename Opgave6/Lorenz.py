41e79cc7db8fdc0773fe4eb4c41cfa99f5fd730c
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-06-02 12:17:57 +0200

    LorenTz

--- Opgave6/Lorenz.py
+++ Opgave6/Lorenz.py
@@ -1,6 +1,6 @@
 from scipy.integrate import odeint
+from scipy.linalg import eig
 import numpy as np
-# from numdifftools import Jacobian
 
 def f2(x,y,z):
     sigma=10
@@ -19,15 +19,10 @@ class Lorenz(object):
         return [self.s*(x[1]-x[0]),x[0]*(self.r-x[2])-x[1],x[0]*x[1]-self.b*x[2]]
     def solve(self,T,dt):
         return odeint(self.f,self.pos,np.arange(0,T,dt))
-#    def df(self,u):
-#       return Jacobian(f2)
-
-L1 = Lorenz([-1,1,0])
-u1 = L1.solve(50,.01)
-L2 = Lorenz([-1.001,1.001,.001])
-u2 = L2.solve(50,.01)
-print(u1[0][0],u2[0][0])
-
-for i in range(0,len(u2)):
-    if i%200==0:
-        print(u2[i])
+    def df(self,u):
+       return np.array([[-self.s, self.s,0],[self.r-u[2],-1,-u[0]],[u[1],u[0],-self.b]])
+    def isStable(self,u):
+        for i in eig(self.df(u))[0].real:
+            if i>=0:
+                return False
+        return True
